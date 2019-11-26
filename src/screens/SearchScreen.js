32b6a6a9f43ae3478ef56c4349ea08214bab3f87
chamikabm <chamika@bitsmedia.com>
2019-11-27 00:02:34 +0800

    Added fix for not showing items when the device screen is small.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, Fragment } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
@@ -16,8 +16,12 @@ const SearchScreen = () => {
     })
   };
 
+  /**
+   * In order to fix the scroll issue with item overflow, we can use empty tags as follows
+   * without the View component with the `flex:1` style property.
+   */
   return (
-      <View>
+      <>
         <SearchBar
             searchTerm={searchTerm}
             onSearchTermChange={setSearchTermChange}
@@ -30,7 +34,7 @@ const SearchScreen = () => {
         }
         {
           results.length > 0 ?
-              <Fragment>
+              <ScrollView>
                 <ResultsList
                     title={'Cost Effective'}
                     results={filterResultsByPrice('$')}
@@ -43,15 +47,21 @@ const SearchScreen = () => {
                     title={'Big Spender'}
                     results={filterResultsByPrice('$$$')}
                 />
-              </Fragment>
+              </ScrollView>
               : null
         }
-      </View>
+      </>
   );
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    // flex: 1,
+    // This tells React Native to Use the full visible screen. Otherwise for some device
+    // screen sizes result list may hidden and won't be able to scroll down even though
+    // we have `ScrollView` component in place, that is because sometimes React Native
+    // uses the space even blow the bottom screen (i.e : Bottom Navigation buttons in Android devices.)
+  }
 });
 
 export default SearchScreen;
